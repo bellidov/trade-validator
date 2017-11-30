7cb3922921b55aff08100bea6482dafcaa882c57
Julio Bellido <bellido.saico.julio.cesar@fisglobal.com>
2017-11-30 16:43:54 -0500

    creating a spring boot starter module

---
+++ starter/src/main/java/com/bellidov/trade/service/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.bellidov.trade.service;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootStarter.class);
+    }
+
+}
